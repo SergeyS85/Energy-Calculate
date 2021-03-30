0ea94ba31e312c8d044e4187455b53a5dfe44afa
Sergey <surzhiks.85@mail.ru>
2021-03-30 21:10:37 +0300

    working app

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-(function(){
-	var button = document.getElementById("toggle-menu");
-	button.addEventListener("click",function(event){
-		event.preventDefault();
-		var menu = document.getElementById("main-menu");
-		menu.classList.toggle("is-open");
-	}); 
-})(); 
+var res = 0;
+var a   = +document.querySelector('.a').value;
+var b   = +document.querySelector('.b').value;
+var c   = +document.querySelector('.c').value;
+var kvt = +document.querySelector('.kvt input').value;
+var res_kvt = document.querySelector('.output');
+var btn = document.querySelector('.btn');
+
+btn.addEventListener('click',function(){
+	if(kvt >= 0 && kvt <= 150) res = kvt * a;
+  if(kvt > 150 && kvt <= 800) res = (150 * a) + (kvt - 150) * b;
+  if(kvt > 800) res = (150 * a) + ((kvt - 150) * b) + (kvt - 800) * c;
+	res_kvt.innerHTML = res;
+})
