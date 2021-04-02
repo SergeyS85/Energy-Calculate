4eb2287b978e5a17a884e23459998e1c333641ae
Sergey <surzhiks.85@mail.ru>
2021-04-02 11:36:54 +0300

    исправлены ошибки с вводом данных в input

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 var res = 0;
-var a   = +document.querySelector('.a').value;
-var b   = +document.querySelector('.b').value;
-var c   = +document.querySelector('.c').value;
-var kvt = +document.querySelector('.kvt input').value;
+var a   = document.querySelector('.a');
+var b   = document.querySelector('.b');
+var c   = document.querySelector('.c');
+var kvt = document.querySelector('.kvt input');
 var res_kvt = document.querySelector('.output');
 var btn = document.querySelector('.btn');
 
 btn.addEventListener('click',function(){
-	if(kvt >= 0 && kvt <= 150) res = kvt * a;
-  if(kvt > 150 && kvt <= 800) res = (150 * a) + (kvt - 150) * b;
-  if(kvt > 800) res = (150 * a) + ((kvt - 150) * b) + (kvt - 800) * c;
+	if(+kvt.value >= 0 && +kvt.value <= 150) res = +kvt.value * +a.value;
+  if(+kvt.value > 150 && +kvt.value <= 800) res = (150 * +a.value) + (+kvt.value - 150) * +b.value;
+  if(+kvt.value > 800) res = (150 * a.value) + ((+kvt.value - 150) * +b.value) + (+kvt.value - 800) * +c.value;
 	res_kvt.innerHTML = res;
+	
 })
