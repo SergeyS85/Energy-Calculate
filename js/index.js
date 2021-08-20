04d458167e098452735db0d3972a527211a5649e
Sergey <surzhiks.85@mail.ru>
2021-08-20 16:38:33 +0300

    change value variable RES

--- js/index.js
+++ js/index.js
@@ -26,6 +26,6 @@ btn.addEventListener('click', function() {
 	if (+kvt.value >= 0 && +kvt.value <= 150) res = +kvt.value * +a.value;
 	if (+kvt.value > 150 && +kvt.value <= 800) res = (150 * +a.value) + (+kvt.value - 150) * +b.value;
 	if (+kvt.value > 800) res = (150 * a.value) + ((+kvt.value - 150) * +b.value) + (+kvt.value - 800) * +c.value;
-	res_kvt.innerHTML = res;
+	res_kvt.innerHTML = res.toFixed(2);
 
 })
