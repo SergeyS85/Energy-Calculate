c5006f3736ecbb8584976ea7f85323cd0c6f2e9c
Sergey <surzhiks.85@mail.ru>
2021-08-22 23:15:59 +0300

    change input

--- js/index.js
+++ js/index.js
@@ -6,20 +6,24 @@ var c = document.querySelector('.c');
 var kvt = document.querySelector('.kvt input');
 var res_kvt = document.querySelector('.output');
 var btn = document.querySelector('.btn');
-
+var search = ['0','1','2','3','4','5']
 inputAll.forEach(function(e) {
-	e.oninput = function(i) {
+	e.oninput = function(event) {
 		var tar = this.value;
+		console.log(tar)
 		var inVal = document.querySelectorAll('input').value;
-		if (tar == '' || tar == undefined) {
-			this.style.border = 'none';
+		// if (tar.length == 0) {
+		// 	this.style.border = null;
+		// }
+		if ((tar.search(/[^\.]/g) == -1) || (tar.search(/\D+/g) == -1)) {
+			this.style.border = '3px solid green';
 		}
-		if (tar.match(/^[\.]|\D+/) == null) {
-			this.style.border = '1px solid green';
-		} else {
+		
+		if((tar.search(/[\.]/g) != -1) || (tar.search(/\d+/g) != -1)) {
 			this.style.border = '1px solid red';
 		}
 	}
+	event.preventDefault()
 })
 
 btn.addEventListener('click', function() {
