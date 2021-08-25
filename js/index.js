e7ef28a73fbd8aafc83ceeded45e3b2b604b7a44
Sergey <surzhiks.85@mail.ru>
2021-08-25 22:03:29 +0300

    evening 25.08.2021

--- js/index.js
+++ js/index.js
@@ -1,36 +1,55 @@
-var res = 0;
-var inputAll = document.querySelectorAll('input');
-var a = document.querySelector('.a');
-var b = document.querySelector('.b');
-var c = document.querySelector('.c');
-var kvt = document.querySelector('.kvt input');
-var res_kvt = document.querySelector('.output');
-var btn = document.querySelector('.btn');
-var search = ['0','1','2','3','4','5']
-inputAll.forEach(function(e) {
-	e.oninput = function(event) {
-		var tar = this.value;
+let res = 0;
+let inputAll = document.querySelectorAll('input');
+let a = document.querySelector('.a');
+let b = document.querySelector('.b');
+let c = document.querySelector('.c');
+let kvt = document.querySelector('.kvt input');
+let res_kvt = document.querySelector('.output');
+let btn = document.querySelector('.btn');
+
+// inputAll.forEach(function(e) {
+// 	e.oninput = function(event) {
+// 		let tar = this.value;
+// 		console.log(tar.search(/[a-z]/gi))
+// 		let inVal = document.querySelectorAll('input').value;
+// 		if (tar.length == 0) {
+// 			this.style.border = '1px solid red';
+// 		}
+// 		// if(tar.search(/^[\d\.]/i) != -1){
+// 		// 	this.style.border = '3px solid red'
+// 		// }
+// 		// // if (tar.match(/(\.+)|\d+/i)) {
+// 		// // 	this.style.border = '3px solid green';
+// 		// // }
 		
-		var inVal = document.querySelectorAll('input').value;
-		// if (tar.length == 0) {
-		// 	this.style.border = null;
-		// }
-		if ((tar.search(/[^\.]/g) == -1) || (tar.search(/\D+/g) == -1)) {
-			this.style.border = '3px solid green';
-		}
-		
-		if((tar.search(/[\.]/g) != -1) || (tar.search(/\d+/g) != -1)) {
-			this.style.border = '1px solid red';
-		}
-	}
+// 		// else{
+// 		// 	this.style.border = '1px solid green'
+// 		// }
+// 	}
 	
+// })
+function checkPhoneForInput(){
+	inputAll.forEach(function(item){
+	item.onfocus = function(event){
+		if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) document.querySelector('.keyboard').style.display = 'flex'
+	}
 })
+}
 
-btn.addEventListener('click', function() {
-	if (+kvt.value >= 0 && +kvt.value <= 150) res = +kvt.value * +a.value;
-	if (+kvt.value > 150 && +kvt.value <= 800) res = (150 * +a.value) + (+kvt.value - 150) * +b.value;
-	if (+kvt.value > 800) res = (150 * a.value) + ((+kvt.value - 150) * +b.value) + (+kvt.value - 800) * +c.value;
-	res_kvt.innerHTML = res.toFixed(2);
+function clickResult(){
+	btn.addEventListener('click', function(e) {
+		if (+kvt.value >= 0 && +kvt.value <= 150) res = +kvt.value * +a.value;
+		if (+kvt.value > 150 && +kvt.value <= 800) res = (150 * +a.value) + (+kvt.value - 150) * +b.value;
+		if (+kvt.value > 800) res = (150 * a.value) + ((+kvt.value - 150) * +b.value) + (+kvt.value - 800) * +c.value;
+		res_kvt.innerHTML = res.toFixed(2);
+  })
+}
 
-})
-document.body.innerHTML += navigator.userAgent
+		
+
+
+function init(){
+	clickResult()
+	checkPhoneForInput()
+}	
+init()
